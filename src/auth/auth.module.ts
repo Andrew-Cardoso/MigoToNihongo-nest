a14a0f5c59e8b45ab67dbd99aba5793b11b78aca
Andrew Cardoso <andrewgcardoso@gmail.com>
2022-05-06 20:39:27 -0300

    removed email module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,11 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JWT } from './strategy/jwt.constants';
 import { JwtStrategy } from './strategy/jwt.strategy';
-import { EmailModule } from 'src/utils/modules/email/email.module';
 import { PrismaModule } from 'src/utils/modules/prisma/prisma.module';
 import { GoogleStrategy } from './strategy/google.strategy';
 
 @Module({
   imports: [
-    EmailModule,
     PrismaModule,
     JwtModule.register({
       secret: JWT.secret,
